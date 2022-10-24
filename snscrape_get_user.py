d1d5e3bce9bfd4fa5c081201aa095ed8c709f01d
sIPs3 <116594985+sIPs3@users.noreply.github.com>
2022-10-24 13:46:37 -0700

    Adding basic scripts

---
+++ snscrape_get_user.py
@@ -0,0 +1,35 @@
+#@title Step 2c: Install script #3
+%%writefile get_user.py
+import os
+import sys
+
+import json
+import re
+
+import snscrape.modules.twitter as sntwitter
+
+def get_userinfo(screen_name):
+  search = sntwitter.TwitterUserScraper(screen_name)
+  entity = search._get_entity()
+
+  if entity is None:
+    print("No user info found")
+    return None
+
+  keys = ["id", "created", "username",
+          "followersCount", "statusesCount", "displayname", "location", "link",
+          "rawDescription", "profileImageUrl"]
+
+  for key in keys:
+    value = entity.__dict__[key]
+    if type(value) == str:
+      value = re.sub("\s", " ", value)
+    print(key+": "+str(value))
+
+  return entity
+
+
+if __name__ == '__main__':
+  username = str(sys.argv[1])
+  get_userinfo(username)
+
---
+++ snscrape_last_active.py
@@ -0,0 +1,37 @@
+import argparse
+import urllib.request
+
+import snscrape.modules.twitter as sntwitter
+
+class LastActive:
+  def mostRecentMention(username):
+    search = sntwitter.TwitterSearchScraper(f'to:{username}')
+    for i,tweet in enumerate(search.get_items()):
+      if tweet.user.username.casefold() == username:
+        return tweet
+
+      if tweet.inReplyToUser is None:
+        continue
+
+      if tweet.inReplyToUser.username.casefold() == username:
+        return tweet
+
+    return None
+
+if __name__ == "__main__":
+  argparser = argparse.ArgumentParser()
+  argparser.add_argument('username')
+
+  args = argparser.parse_args()
+
+  username = args.username.casefold().strip('@')
+
+
+  tweet = LastActive.mostRecentMention(args.username.casefold())
+
+  if tweet is not None:
+    print(tweet)
+    print(tweet.date)
+  else:
+    print("UNKNOWN")
+
